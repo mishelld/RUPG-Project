1df15e1fc5598de57307bce6745b12bac984c86e
mishelld <mishelldub@gmail.com>
2026-02-01 16:23:19 +0200

    feat: Add `getMainAndFriends` method to process generated users and specify fields for user generation.

--- model.js
+++ model.js
@@ -1,7 +1,9 @@
 class Generator {
   async generateUsers() {
     try {
-      const response = await fetch(`https://randomuser.me/api/?results=7`);
+      const response = await fetch(
+        `https://randomuser.me/api/?results=7&inc=picture,name,location`,
+      );
       if (!response.ok) {
         throw new Error("User not found");
       }
@@ -11,4 +13,31 @@ class Generator {
       console.error("Error fetching users:", error.message);
     }
   }
+  async getMainAndFriends() {
+    try {
+      const users = await this.generateUsers();
+      const mainUser = users[0];
+      const mainUserData = {
+        firstName: mainUser.name.first,
+        lastName: mainUser.name.last,
+        picture: mainUser.picture.large,
+        state: mainUser.location.state,
+        city: mainUser.location.city,
+      };
+      const friends = users.slice(1);
+      const friendsData = friends.map((f) => ({
+        firstName: f.name.first,
+        lastName: f.name.last,
+      }));
+      return { mainUserData, friendsData };
+    } catch (error) {
+      console.error("Error fetching users:", error.message);
+    }
+  }
 }
+/*
+const generator = new Generator();
+
+const users = await generator.getMainAndFriends();
+console.log(users);
+*/
